fbe54d6a4e01430fb7a9cf7732bf4700cb4de9fa
Justas Titovec <justas.titovec@yahoo.com>
2020-12-28 15:16:25 +0100

    UserHeafer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,9 @@
 import { combineReducers } from 'redux';
 
+import postReducer from './postReducer';
+import userReducer from './usersReducer';
+
 export default combineReducers({
-  replaceMe: () => 'Hi there',
+  posts: postReducer,
+  users: userReducer,
 });
